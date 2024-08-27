3af151b2835d79764cd222524c46059751c8faa6
kareemAlqubati <salamkareem792@gmail.com>
2024-08-27 13:15:28 +0300

    Compelet

--- index.js
+++ index.js
@@ -1,5 +1,12 @@
 function isPalindrome(word) {
-  // Write your algorithm here
+  word = word.toLowerCase();
+
+  for (let i = 0; i < word.length / 2; i++) {
+    if (word[i] !== word[word.length - 1 - i]) {
+      return false;
+    }
+  }
+  return true;
 }
 
 /* 
